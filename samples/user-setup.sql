72dc329516b2b58c19cd78379250fc848a9830c8
Tobias Florek <me@ibotty.net>
2024-02-14 19:50:15 +0100

    remove leftover search path

--- samples/user-setup.sql
+++ samples/user-setup.sql
@@ -1,4 +1,3 @@
 CREATE USER "bot-download-proxy";
-ALTER USER "bot-download-proxy" SET search_path TO data_gateways;
 GRANT EXECUTE ON FUNCTION download_proxy_file_info TO "bot-download-proxy";
 GRANT INSERT ON download_proxy_access_log TO "bot-download-proxy";
