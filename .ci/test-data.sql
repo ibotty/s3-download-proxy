00a04dc48e60868514ddfd5964913b9d128e5930
Tobias Florek <me@ibotty.net>
2024-02-14 20:15:53 +0100

    properly set env variables in ci

--- .ci/test-data.sql
+++ .ci/test-data.sql
@@ -2,6 +2,6 @@ ALTER USER "bot-download-proxy" PASSWORD 'pass';
 ALTER TABLE download_proxy_files ALTER COLUMN aws_endpoint_url SET DEFAULT '${AWS_ENDPOINT_URL}';
 ALTER TABLE download_proxy_files ALTER COLUMN aws_region SET DEFAULT '${AWS_REGION}';
 ALTER TABLE download_proxy_files ALTER COLUMN s3_bucket SET DEFAULT '${S3_BUCKET}';
-ALTER TABLE download_proxy_files ALTER COLUMN aws_s3_force_path_style SET DEFAULT TRUE;
+ALTER TABLE download_proxy_files ALTER COLUMN aws_s3_force_path_style SET DEFAULT ${AWS_S3_FORCE_PATH_STYLE};
 
 SELECT create_download_proxy_link('${S3_BUCKET}', 'test-key', 'test.txt');
